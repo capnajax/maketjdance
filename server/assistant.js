cf6bdaffddf16abba7daff04b855e5c330b99d58
Thomas Wilkinson <thomas@capnajax.com>
2018-05-31 02:30:22 +0000

    make tj happy

--- server/assistant.js
+++ server/assistant.js
@@ -43,7 +43,7 @@ var assistant = serviceManager.get("watson_assistant"),
     	"dance" : { sad: "sulk", happy: "dance" },
     },
 
-    feeling = "sad";
+    feeling = "happy";
 
 module.exports = {
 	assist: (text, cb) => {
